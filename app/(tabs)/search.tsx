b443e764c01787edf3433a5261e4d8f9dadf5dbc
Josué Hernández <joheca1998@outlook.com>
2025-03-21 15:43:10 -0600

    Develop Search Screen

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,138 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { useState, useEffect } from "react";
+import { View, Text, ActivityIndicator, FlatList, Image, StyleSheet } from "react-native";
 
-const search = () => {
+import { useTheme } from '@/context/ThemeProvider'; 
+import { ThemeColors } from 'ThemeTypes';
+
+import { icons } from "@/constants/icons";
+
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+
+import SearchBar from "@/components/SearchBar";
+import MovieDisplayCard from "@/components/MovieCard";
+
+const Search = () => {
+  const { theme } = useTheme();
+  const styles = createStyles(theme);
+
+  // Function to create styles
+  function createStyles(theme: ThemeColors) {
+    return StyleSheet.create({
+      container: {
+        backgroundColor: theme.background,
+      },
+      text: {
+        color: theme.onBackground,
+      },
+      error: {
+        color: theme.error,
+      },
+      primaryText: {
+        color: theme.primary
+      },
+      notFoundText: {
+        color: theme.disabled
+      }
+    });
+  }
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const {
+    data: movies = [],
+    loading,
+    error,
+    refetch: loadMovies,
+    reset,
+  } = useFetch(() => fetchMovies({ query: searchQuery }), false);
+
+  const handleSearch = (text: string) => {
+    setSearchQuery(text);
+  };
+
+  // Debounced search effect
+  useEffect(() => {
+    const timeoutId = setTimeout(async () => {
+      if (searchQuery.trim()) {
+        await loadMovies();
+
+      } else {
+        reset();
+      }
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
+  }, [searchQuery]);
+
   return (
-    <View>
-      <Text>search</Text>
-    </View>
-  )
-}
+    <View style={styles.container} className="flex-1">
 
-export default search
+      <FlatList
+        className="px-5"
+        data={movies as Movie[]}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <MovieDisplayCard {...item} />}
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "flex-start",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image source={icons.logo} className="w-12 h-12" />
+            </View>
+
+            <View className="my-5">
+              <SearchBar
+                placeholder="Search for a movie"
+                value={searchQuery}
+                onChangeText={handleSearch}
+              />
+            </View>
+
+            {loading && (
+              <ActivityIndicator
+                size="large"
+                color={`${theme.primary}`}
+                className="my-3"
+              />
+            )}
+
+            {error && (
+              <Text style={styles.error} className="px-5 my-3">
+                Error: {error.message}
+              </Text>
+            )}
+
+            {!loading &&
+              !error &&
+              searchQuery.trim() &&
+              movies?.length! > 0 && (
+                <Text style={styles.text} className="text-xl font-bold">
+                  Search Results for{" "}
+                  <Text style={styles.primaryText} className="text-accent">{searchQuery}</Text>
+                </Text>
+              )}
+          </>
+        }
+        ListEmptyComponent={
+          !loading && !error ? (
+            <View className="mt-10 px-5">
+              <Text style={styles.notFoundText} className="text-center">
+                {searchQuery.trim()
+                  ? "No movies found"
+                  : "Start typing to search for movies"}
+              </Text>
+            </View>
+          ) : null
+        }
+      />
+    </View>
+  );
+};
+
+export default Search;
