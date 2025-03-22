e21d754036fac166e6cfe7d23059c4b8b6800f9b
Josué Hernández <joheca1998@outlook.com>
2025-03-21 19:27:49 -0600

    Creating a Save page and a Profile page for future development

--- app/(tabs)/save.tsx
+++ app/(tabs)/save.tsx
@@ -1,12 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { icons } from "@/constants/icons";
+import { View, Text, Image, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const save = () => {
-  return (
-    <View>
-      <Text>save</Text>
-    </View>
-  )
+import { useTheme } from '@/context/ThemeProvider'; 
+import { ThemeColors } from 'ThemeTypes';
+
+// Function to create styles
+function createStyles(theme: ThemeColors) {
+  return StyleSheet.create({
+    container: {
+      backgroundColor: theme.background,
+    },
+    text: {
+      color: theme.onBackground,
+    },
+  });
 }
 
-export default save
+
+const Save = () => {
+  const { theme } = useTheme();
+  const styles = createStyles(theme);
+    
+  return (
+    <SafeAreaView style={styles.container} className="flex-1 px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.save} className="size-8" tintColor={`${theme.primary}`} />
+        <Text style={styles.text} className="text-lg font-bold">Save</Text>
+        <Text style={styles.text}className="text-sm mt-2">This page is currently under development.</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Save;
